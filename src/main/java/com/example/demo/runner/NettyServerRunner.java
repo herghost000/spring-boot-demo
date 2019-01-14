7d28b8a82353ab219b84d07632402aba68f2cafb
ZackAddy <77209302@qq.com>
2019-01-14 21:03:40 +0800

    add netty

--- src/main/java/com/example/demo/runner/NettyServerRunner.java
+++ src/main/java/com/example/demo/runner/NettyServerRunner.java
@@ -1,13 +1,12 @@
 package com.example.demo.runner;
 
+import com.example.demo.netty.listener.NettyServerListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
-import java.util.Arrays;
 
 /**
  * https://github.com/marquisXuan/netty/blob/master/spring-boot-netty/netty-demo/netty-server/src/main/java/org/yyx/netty/server/dispatcher/RequestDispatcher.java
@@ -15,12 +14,13 @@ import java.util.Arrays;
 @Component
 public class NettyServerRunner implements CommandLineRunner {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-//    @Resource
-//    private NettyServerListener nettyServerListener;
+    @Resource
+    private NettyServerListener nettyServerListener;
 
     @Override
     public void run(String... args) throws Exception {
-
+        System.out.println("66666666666666666666666");
+        nettyServerListener.start();
     }
 }
 
